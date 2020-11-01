b6c327b03d3524979c689ab8e7af3f0327df7ea8
TheRealJackiBoi <jslam@oulund.dk>
2020-11-01 23:47:01 +0100

    Main Component start
    Main Component start

---
+++ myapp/app.py
@@ -0,0 +1,11 @@
+from flask import Flask,redirect,url_for,render_template,request
+
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def home():
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    #DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(port=8080,debug=True)
