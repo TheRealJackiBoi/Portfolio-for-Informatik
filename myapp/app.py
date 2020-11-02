3a26fc810d016b52c07ea9a08347578b80000f70
TheRealJackiBoi <jslam@oulund.dk>
2020-11-02 02:29:40 +0100

    added new files and edited kontakt
    added opgaver.html, emner.html, kontakt.html
    
    and developed on kontakt
    
    created routes for all files

--- myapp/app.py
+++ myapp/app.py
@@ -1,10 +1,28 @@
 from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
+
 @app.route('/',methods=['GET','POST'])
 def home():
     
     return render_template('index.html')
+
+
+@app.route('/opgaver',methods=['GET','POST'])
+def opgaver():
+
+   return render_template('opgaver.html')
+
+@app.route('/emner',methods=['GET','POST'])
+def emner():
+
+   return render_template('emner.html')
+
+@app.route('/kontakt',methods=['GET','POST'])
+def kontakt():
+
+   return render_template('kontakt.html')
+
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
